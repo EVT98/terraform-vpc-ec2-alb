9a20571c7fe668db07ef381d7dc1fff8ab506a6b
EVT98 <e.vitateka@gmail.com>
2026-01-11 17:55:27 +0100

    changes

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "server1" {
   ami = "ami-02d7fd1c2af6eead0"
   instance_type = var.INSTANCE_TYPE
-  availability_zone = "${var.REGION}a" 
+  availability_zone = "${var.REGION}-a" 
   subnet_id = aws_subnet.Private1.id
   vpc_security_group_ids = [aws_security_group.sg1.id]
   user_data = file("code.sh")
@@ -15,7 +15,7 @@ resource "aws_instance" "server1" {
 resource "aws_instance" "server2" {
   ami = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.micro"
-  availability_zone = "us-east-1b"
+  availability_zone = "${var.REGION}-b"
   subnet_id = aws_subnet.Private2.id
   vpc_security_group_ids = [aws_security_group.sg1.id]
   user_data = file("code.sh")
