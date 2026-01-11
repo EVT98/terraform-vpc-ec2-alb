e644491069d7316950545ecd9e15f8684f353d91
EVT98 <e.vitateka@gmail.com>
2026-01-11 17:26:29 +0100

    variables changes

--- variable.tf
+++ variable.tf
@@ -1,11 +1,14 @@
 variable "ENVIRONMENT" {
-  
+  type = string
+  default = "dev"
 }
 
 variable "REGION" {
-  
+  type = string
+  default = "us-east-1"
 }
 
 variable "INSTANCE_TYPE" {
-  
+  type = string
+  default = "t2.micro"
 }
